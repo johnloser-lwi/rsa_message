680bcc9bfcd25ac4e165e654740584d98a552ef6
johnloser-lwi <john-masterchief@hotmail.com>
2023-09-09 11:42:43 +0800

    fix imports. add key file cache.

--- rsa_message.py
+++ rsa_message.py
@@ -6,99 +6,110 @@ from cryptography.hazmat.primitives.asymmetric import padding
 
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askopenfilename
+
+import pyperclip
 
 import base64
 
-def generate_key_pair():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=1024,  # RSA1024
-        backend=default_backend()
-    )
-    return private_key, private_key.public_key()
+class rsa_message:
+    def __init__(self):
+        self.current_public_key = ""
+        self.current_private_key = ""
 
-def encrypt_message(public_key, message):
-    try:
+    def generate_key_pair(self):
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=1024,  # RSA1024
+            backend=default_backend()
+        )
+        return private_key, private_key.public_key()
+
+    def encrypt_message(self, public_key, message):
+        try:
+            
+            message_bytes = message.encode('utf-8')
+            ciphertext = public_key.encrypt(
+                message_bytes,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA1()),
+                    algorithm=hashes.SHA1(),
+                    label=None
+                )
+            )
+            return ciphertext
+        except:
+            return "Failed to encrypt message!"
+
+    def decrypt_message(self, private_key, ciphertext):
+        try:
+            plaintext = private_key.decrypt(
+                ciphertext,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA1()),
+                    algorithm=hashes.SHA1(),
+                    label=None
+                )
+            )
+            return plaintext.decode('utf-8')
+        except:
+            return "Invalid message!"
+
+    def run_encrypt(self):
+        # open file dialoge to load public key
+        public_key = None
+        Tk().withdraw()
+        if self.current_public_key == "":
+            self.current_public_key = askopenfilename(title="Select a Public Key File", filetypes=[("Public Key Files", "*.pem")])
+        try:
+            with open(self.current_public_key, "rb") as f:
+                    public_key = serialization.load_pem_public_key(
+                        f.read(),
+                        backend=default_backend()
+                    )
+        except:
+            print("Invalid key file!")
+            return
+        msg = input("Enter the message: ")
+        encrypted_message = self.encrypt_message(public_key, msg)
+        print("Encrypted Message:", base64.b64encode(encrypted_message).decode('utf-8'))
+        # copy message to clipboard
+        pyperclip.copy(base64.b64encode(encrypted_message).decode('utf-8'))
+        print("Message copied to clipboard")
         
-        message_bytes = message.encode('utf-8')
-        ciphertext = public_key.encrypt(
-            message_bytes,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA1()),
-                algorithm=hashes.SHA1(),
-                label=None
-            )
-        )
-        return ciphertext
-    except:
-        return "Failed to encrypt message!"
-
-def decrypt_message(private_key, ciphertext):
-    try:
-        plaintext = private_key.decrypt(
-            ciphertext,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA1()),
-                algorithm=hashes.SHA1(),
-                label=None
-            )
-        )
-        return plaintext.decode('utf-8')
-    except:
-        return "Invalid message!"
-
-def run_encrypt():
-    # open file dialoge to load public key
-    public_key = None
-    Tk().withdraw()
-    filename = askopenfilename(title="Select a Public Key File", filetypes=[("Public Key Files", "*.pem")])
-    try:
-        with open(filename, "rb") as f:
-                public_key = serialization.load_pem_public_key(
-                    f.read(),
-                    backend=default_backend()
-                )
-    except:
-        print("Invalid key file!")
-        return
-    msg = input("Enter the message: ")
-    encrypted_message = encrypt_message(public_key, msg)
-    print("Encrypted Message:", base64.b64encode(encrypted_message).decode('utf-8'))
-    # copy message to clipboard
-    import pyperclip
-    pyperclip.copy(base64.b64encode(encrypted_message).decode('utf-8'))
-    print("Message copied to clipboard")
-    
-def run_decrypt():
-    # open file dialoge to load public key
-    private_key = None
-    Tk().withdraw()
-    filename = askopenfilename(title="Select a Private Key File", filetypes=[("Private Key Files", "*.pem")])
-    try:
-        with open(filename, "rb") as f:
-                private_key = serialization.load_pem_private_key(
-                    f.read(),
-                    password=None,
-                    backend=default_backend()
-                )
-    except:
-        print("Invalid key file!")
-        return    
-    msg = input("Enter the message: ")
-    decrypted_message = decrypt_message(private_key, base64.b64decode(msg))
-    print("Decrypted Message:", decrypted_message)
+    def run_decrypt(self):
+        # open file dialoge to load public key
+        private_key = None
+        Tk().withdraw()
+        if self.current_private_key == "":
+            self.current_private_key = askopenfilename(title="Select a Private Key File", filetypes=[("Private Key Files", "*.pem")])
+        try:
+            with open(self.current_private_key, "rb") as f:
+                    private_key = serialization.load_pem_private_key(
+                        f.read(),
+                        password=None,
+                        backend=default_backend()
+                    )
+        except:
+            print("Invalid key file!")
+            return    
+        msg = input("Enter the message: ")
+        decrypted_message = self.decrypt_message(private_key, base64.b64decode(msg))
+        print("Decrypted Message:", decrypted_message)
 
 if __name__ == "__main__":
     
+    app = rsa_message()
+    
     while True:
     
-        print("1. Encrypt Message\n2. Decrypt Message\n3. Generate Key Pair\n4. Exit")
-        operation = input("Enter the operation(1/2/3/4): ")
+        print("1. Encrypt Message\n2. Decrypt Message\n3. Generate Key Pair\n4. Reset Key Selections\n5. Exit")
+        operation = input("Enter the operation(1/2/3/4/5): ")
         
         if operation == '1':
-            run_encrypt()
+            app.run_encrypt()
         elif operation == '2':
-            run_decrypt()
+            app.run_decrypt()
         elif operation == '3':
             private_key, public_key = generate_key_pair()
             # open dialogue to select folder to save keys
@@ -116,8 +127,10 @@ if __name__ == "__main__":
                     encoding=serialization.Encoding.PEM,
                     format=serialization.PublicFormat.SubjectPublicKeyInfo
                 ))
-                
         elif operation == '4':
+            app.current_public_key = ""
+            app.current_private_key = ""
+        elif operation == '5':
             break
         else:
             print("Invalid operation!")
