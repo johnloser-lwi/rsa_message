87af649b2f9723c6c40a007c63740e5766c38d35
johnloser-lwi <john-masterchief@hotmail.com>
2023-09-12 14:58:02 +0800

    fix file formatting

--- rsa_message.py
+++ rsa_message.py
@@ -12,6 +12,7 @@ import pyperclip
 
 import base64
 
+
 class rsa_message:
     def __init__(self):
         self.current_public_key = ""
@@ -27,7 +28,7 @@ class rsa_message:
 
     def encrypt_message(self, public_key, message):
         try:
-            
+
             message_bytes = message.encode('utf-8')
             ciphertext = public_key.encrypt(
                 message_bytes,
@@ -58,55 +59,53 @@ class rsa_message:
     def run_encrypt(self, msg=""):
         # open file dialoge to load public key
         public_key = None
-        #tk.Tk().withdraw()
         if self.current_public_key == "":
-            self.current_public_key = askopenfilename(title="Select a Public Key File", filetypes=[("Public Key Files", "*.pem")])
+            self.current_public_key = askopenfilename(title="Select a Public Key File",
+                                                      filetypes=[("Public Key Files", "*.pem")])
         try:
             with open(self.current_public_key, "rb") as f:
-                    public_key = serialization.load_pem_public_key(
-                        f.read(),
-                        backend=default_backend()
-                    )
+                public_key = serialization.load_pem_public_key(
+                    f.read(),
+                    backend=default_backend()
+                )
         except:
             print("Invalid key file!")
             return ""
-        if msg == "" : msg = input("Enter the message: ")
+        if msg == "": msg = input("Enter the message: ")
         encrypted_message = self.encrypt_message(public_key, msg)
         out = base64.b64encode(encrypted_message).decode('utf-8')
         print("Encrypted Message:", out)
         # copy message to clipboard
         pyperclip.copy(out)
         print("Message copied to clipboard")
-        
+
         return out
-        
+
     def run_decrypt(self, msg=""):
         # open file dialoge to load public key
         private_key = None
-        #tk.Tk().withdraw()
         if self.current_private_key == "":
-            self.current_private_key = askopenfilename(title="Select a Private Key File", filetypes=[("Private Key Files", "*.pem")])
+            self.current_private_key = askopenfilename(title="Select a Private Key File",
+                                                       filetypes=[("Private Key Files", "*.pem")])
         try:
             with open(self.current_private_key, "rb") as f:
-                    private_key = serialization.load_pem_private_key(
-                        f.read(),
-                        password=None,
-                        backend=default_backend()
-                    )
+                private_key = serialization.load_pem_private_key(
+                    f.read(),
+                    password=None,
+                    backend=default_backend()
+                )
         except:
             print("Invalid key file!")
-            return ""  
-        if msg == "" : msg = input("Enter the message: ")
+            return ""
+        if msg == "": msg = input("Enter the message: ")
         decrypted_message = self.decrypt_message(private_key, base64.b64decode(msg))
         print("Decrypted Message:", decrypted_message)
-        
+
         return decrypted_message
-        
+
     def run_generate_key_pair(self):
         private_key, public_key = self.generate_key_pair()
         # open dialogue to select folder to save keys
-        
-        #tk.Tk().withdraw()
         folder = askdirectory(title="Select a folder to save keys")
         with open(folder + "/private_key.pem", "wb") as f:
             f.write(private_key.private_bytes(
@@ -119,7 +118,7 @@ class rsa_message:
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
             ))
-            
+
     def reset_key_file_selections(self):
         self.current_private_key = ""
         self.current_public_key = ""
@@ -130,81 +129,84 @@ class RSA_GUI:
         self.root = root
         self.root.title("RSA Messenger")
         self.app = rsa_message()
-        
-        self.message_input = tk.Entry(root, width=60, validate="key", validatecommand=(root.register(self.validate_input), '%P'))
+
+        self.message_input = tk.Entry(root, width=60, validate="key",
+                                      validatecommand=(root.register(self.validate_input), '%P'))
         self.message_output = tk.Text(root, width=50, wrap=tk.WORD, height=2)
-        
+
         # Create buttons for each operation
         encrypt_button = tk.Button(root, text="Encrypt Message", command=self.run_encrypt)
         decrypt_button = tk.Button(root, text="Decrypt Message", command=self.run_decrypt)
         generate_key_button = tk.Button(root, text="Generate Key Pair", command=self.run_generate_key_pair)
         reset_button = tk.Button(root, text="Reset Key Selections", command=self.reset_key_file_selections)
         exit_button = tk.Button(root, text="Exit", command=root.quit)
-        
+
         # Arrange buttons in a grid
         self.message_input.grid(row=1, column=0, columnspan=2, pady=10)
         encrypt_button.grid(row=1, column=3, pady=10, padx=10)
-        
+
         self.message_output.grid(row=2, column=0, columnspan=2, padx=20, pady=10)
         decrypt_button.grid(row=2, column=3, pady=10, padx=10)
-        
+
         generate_key_button.grid(row=3, column=0, pady=10)
         reset_button.grid(row=3, column=1, pady=10)
-        #exit_button.grid(row=4, column=0, pady=10)
-    
+        # exit_button.grid(row=4, column=0, pady=10)
+
     def validate_input(self, P):
         # Ensure the input does not exceed 86 characters
-        
+
         # chinese characters are 3 bytes long
         l = 0
         for c in P:
-            if ord(c) > 255 : l += 3
-            else : l += 1
-        
+            if ord(c) > 255:
+                l += 3
+            else:
+                l += 1
+
         return l <= 86
-    
+
     def run_encrypt(self):
         print("Running encryption...")
         # Implement your encryption code here
         # get message from input
         msg = self.message_input.get()
-        
+
         res = self.app.run_encrypt(msg)
         # set message output text
-        if res != "" : 
+        if res != "":
             self.message_output.delete("1.0", tk.END)
             self.message_output.insert(tk.END, "Copied to clipboard!")
-        else : 
+        else:
             self.message_output.delete("1.0", tk.END)
             self.message_output.insert(tk.END, "Failed to encrypt message!")
-    
+
     def run_decrypt(self):
         print("Running decryption...")
         # Implement your decryption code here
         # get message from clipboard
         msg = pyperclip.paste()
         res = self.app.run_decrypt(msg)
-        if res != "" : 
+        if res != "":
             self.message_output.delete("1.0", tk.END)
             self.message_output.insert(tk.END, res)
-        else : 
+        else:
             self.message_output.delete("1.0", tk.END)
             self.message_output.insert(tk.END, "Failed to encrypt message!")
-    
+
     def run_generate_key_pair(self):
         print("Running key pair generation...")
         # Implement your key pair generation code here
         self.app.run_generate_key_pair()
-    
+
     def reset_key_file_selections(self):
         print("Resetting key selections...")
         # Implement your key selection reset code here
         self.app.reset_key_file_selections()
 
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = RSA_GUI(root)
     root.mainloop()
-    
+
     print("Closed!")
-    
